bdfa7d5446046274077409738a4a53ccc5a43897
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-23 18:24:09 -0300

    Generate scaffold payments

---
+++ spec/views/payments/edit.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "payments/edit", type: :view do
+  let(:payment) {
+    Payment.create!(
+      name: "MyString",
+      amount: 1,
+      user: nil
+    )
+  }
+
+  before(:each) do
+    assign(:payment, payment)
+  end
+
+  it "renders the edit payment form" do
+    render
+
+    assert_select "form[action=?][method=?]", payment_path(payment), "post" do
+
+      assert_select "input[name=?]", "payment[name]"
+
+      assert_select "input[name=?]", "payment[amount]"
+
+      assert_select "input[name=?]", "payment[user_id]"
+    end
+  end
+end
