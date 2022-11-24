bdaa92d704d507c35bd005204002dd5e3a10b94a
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-24 00:04:13 -0300

    Update migrations

--- db/migrate/20221123210212_create_categories.rb
+++ db/migrate/20221123210212_create_categories.rb
@@ -3,7 +3,6 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.string :name
       t.string :icon
-      t.datetime :created_at
 
       t.timestamps
     end
--- db/migrate/20221123211205_create_payments.rb
+++ db/migrate/20221123211205_create_payments.rb
@@ -3,7 +3,6 @@ class CreatePayments < ActiveRecord::Migration[7.0]
     create_table :payments do |t|
       t.string :name
       t.integer :amount
-      t.datetime :created_at
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
