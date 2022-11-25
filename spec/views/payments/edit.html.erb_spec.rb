f7ae5cd0b642857724bc5b9f561351b66ed8bbd4
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-25 01:19:04 -0300

    Fix Linters

--- spec/views/payments/edit.html.erb_spec.rb
+++ spec/views/payments/edit.html.erb_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "payments/edit", type: :view do
-  let(:payment) {
+RSpec.describe 'payments/edit', type: :view do
+  let(:payment) do
     Payment.create!(
-      name: "MyString",
+      name: 'MyString',
       amount: 1,
       user: nil
     )
-  }
+  end
 
   before(:each) do
     assign(:payment, payment)
   end
 
-  it "renders the edit payment form" do
+  it 'renders the edit payment form' do
     render
 
-    assert_select "form[action=?][method=?]", payment_path(payment), "post" do
+    assert_select 'form[action=?][method=?]', payment_path(payment), 'post' do
+      assert_select 'input[name=?]', 'payment[name]'
 
-      assert_select "input[name=?]", "payment[name]"
+      assert_select 'input[name=?]', 'payment[amount]'
 
-      assert_select "input[name=?]", "payment[amount]"
-
-      assert_select "input[name=?]", "payment[user_id]"
+      assert_select 'input[name=?]', 'payment[user_id]'
     end
   end
 end
