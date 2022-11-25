2151ea67228dd044e2a8e88c43a622c138b931ca
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-11-25 01:08:52 -0300

    Fix validation

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,5 +4,4 @@ class Payment < ApplicationRecord
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :category_id, presence: true
 end
