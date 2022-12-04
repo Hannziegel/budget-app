65c53556a72058a9b71e6cfda15542d6cf0d4900
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-12-03 22:19:44 -0300

    Add testing

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Test_name', email: 'test_email@gmail.com', password: 'testpassword', password_confirmation: 'testpassword')
+    @category = Category.create(name: 'test_category', user_id: subject.id)
+    @transaction = Payment.create(name: 'test_payment', amount: 24, user_id: @user.id)
+  end
+
+  it 'Inventoryfood should have valid attributes' do
+    expect(@transaction.name).to eq('test_payment')
+  end
+
+  it 'Inventoryfood should have valid attributes' do
+    expect(@transaction.amount).to eq(24)
+  end
+
+  it 'user for inventory food should be valid' do
+    expect(@user).to be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Test_name', email: 'test_email@gmail.com', password: 'testpassword', password_confirmation: 'testpassword')
+  end
+
+  it 'User should have valid name attribute' do
+    expect(@user.name).to eq('Test_name')
+  end
+
+  it 'User should have valid email attribute' do
+    expect(@user.email).to eq('test_email@gmail.com')
+  end
+
+  it 'user should have valid password' do
+    expect(@user.password).to eq('testpassword')
+  end
+
+  it 'user should have valid password confirmation' do
+    expect(@user.password_confirmation).to eq('testpassword')
+  end
+
+  it 'user for inventory food should be valid' do
+    expect(@user).to be_valid
+  end
 end
